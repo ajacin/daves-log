c0eb5ae99f715dc4a06a83e6417ab834b1668399
“Arun <arunjacob2@gmail.com>
2024-01-05 17:23:43 -0500

    time ago in view page

--- src/components/ActivityTime.jsx
+++ src/components/ActivityTime.jsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
+import ReactTimeAgo from "react-time-ago";
 
-const ActivityTime = ({ activityName,activityTime }) => {
+const ActivityTime = ({ activityName, activityTime }) => {
   const getTimeDifference = (activityTime) => {
     const now = new Date();
     const activityDate = new Date(activityTime);
-    const timeDifference = ((now - activityDate) / (1000 * 60 * 60));  // returns time difference in hours
+    const timeDifference = (now - activityDate) / (1000 * 60 * 60); // returns time difference in hours
     return timeDifference;
-  }
+  };
 
-  const [timeDifference, setTimeDifference] = useState(() => getTimeDifference(activityTime));
+  const [timeDifference, setTimeDifference] = useState(() =>
+    getTimeDifference(activityTime)
+  );
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -18,32 +21,14 @@ const ActivityTime = ({ activityName,activityTime }) => {
     return () => clearInterval(intervalId); // Clear interval on unmount
   }, [activityTime]);
 
-  const renderTime = () => {
-    if (timeDifference < 1) {
-      return "Less than an hour";
-    } else if (timeDifference < 2) {
-      return "1 - 2 hours";
-    } else if (timeDifference < 3) {
-      return "2 - 3 hours";
-    } else if (timeDifference < 4) {
-      return "3 - 4 hours";
-    } else if (timeDifference < 5) {
-      return "4 - 5 hours";
-    } else if (timeDifference < 24) {
-      return "More than 5 hours";
-    } else {
-      return "More than a day";
-    }
-  }
-
   const cutOffTime =
-      activityName === "Feed"
-        ? 3
-        : activityName === "Diaper"
-        ? 5
-        : activityName === "Vitamin D"
-        ? 24
-        : 24;
+    activityName === "Feed"
+      ? 3
+      : activityName === "Diaper"
+      ? 5
+      : activityName === "Vitamin D"
+      ? 24
+      : 24;
 
   return (
     <div
@@ -51,7 +36,8 @@ const ActivityTime = ({ activityName,activityTime }) => {
         timeDifference > cutOffTime ? "text-red-500" : "text-green-500"
       }`}
     >
-      {renderTime()} / {cutOffTime} hours
+      <ReactTimeAgo date={activityTime} locale="en-US" />
+      {/* <p> / {cutOffTime} hours </p> */}
     </div>
   );
 };
