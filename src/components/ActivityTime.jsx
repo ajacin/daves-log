96c0a4c38c66a8884b34046c8389bc48b1beb0a1
“Arun <arunjacob2@gmail.com>
2024-01-05 21:22:28 -0500

    eslint fixes

--- src/components/ActivityTime.jsx
+++ src/components/ActivityTime.jsx
@@ -1,38 +1,43 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback, useMemo } from "react";
 import ReactTimeAgo from "react-time-ago";
 
 const ActivityTime = ({ activityName, activityTime }) => {
-  const activityDate = new Date(activityTime);
-  const getTimeDifference = (activityTime) => {
+  const activityDate = useMemo(() => new Date(activityTime), [activityTime]);
+
+  const getTimeDifference = useCallback(() => {
     const now = new Date();
     const timeDifference = (now - activityDate) / (1000 * 60 * 60); // returns time difference in hours
     return timeDifference;
-  };
+  }, [activityDate]);
 
   const [timeDifference, setTimeDifference] = useState(() =>
-    getTimeDifference(activityTime)
+    getTimeDifference()
   );
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setTimeDifference(getTimeDifference(activityTime));
+      setTimeDifference(getTimeDifference());
     }, 1000);
 
     return () => clearInterval(intervalId); // Clear interval on unmount
-  }, [activityTime, getTimeDifference]);
+  }, [getTimeDifference]);
 
-  const cutOffTime =
-    activityName === "Feed"
-      ? 3
-      : activityName === "Diaper"
-      ? 5
-      : activityName === "Vitamin D"
-      ? 24
-      : 24;
+  const cutOffTime = useMemo(() => {
+    switch (activityName) {
+      case "Feed":
+        return 3;
+      case "Diaper":
+        return 5;
+      case "Vitamin D":
+        return 24;
+      default:
+        return 24;
+    }
+  }, [activityName]);
 
   return (
     <div
-      className={` ${
+      className={`${
         timeDifference > cutOffTime ? "text-red-500" : "text-green-500"
       }`}
     >
