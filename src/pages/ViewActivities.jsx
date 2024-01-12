4161f7bac4e9039b54ac932c879a97c8f75138bf
“Arun <arunjacob2@gmail.com>
2024-01-11 23:51:40 -0500

    view page title

--- src/pages/ViewActivities.jsx
+++ src/pages/ViewActivities.jsx
@@ -116,9 +116,7 @@ export function ViewActivities() {
   return (
     <div className="container mx-auto p-4 sm:px-6 lg:px-8">
       <section className="mt-12">
-        <h2 className="text-2xl font-bold mb-4 text-gray-800">
-          Latest Activities
-        </h2>
+        <h2 className="text-2xl font-bold mb-4 text-gray-800">Last 24 hours</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {activities.current
             .sort((a, b) => new Date(b.activityTime) - new Date(a.activityTime))
