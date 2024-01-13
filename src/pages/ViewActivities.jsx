1c70beac75da01bde9e6dca1474b3958d53d2182
“Arun <arunjacob2@gmail.com>
2024-01-13 03:14:26 -0500

    clock commented to test

--- src/pages/ViewActivities.jsx
+++ src/pages/ViewActivities.jsx
@@ -145,7 +145,7 @@ export function ViewActivities() {
           <h2 className="text-2xl font-bold mb-4 text-gray-800">
             Last 24 hours
           </h2>
-          <Clock></Clock>
+          {/* <Clock></Clock> */}
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {activities.current
