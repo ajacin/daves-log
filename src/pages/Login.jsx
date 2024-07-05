fba248773efbad69efab82794ec3c1e7423eb2b3
“Arun <arunjacob2@gmail.com>
2024-07-05 15:38:05 -0400

    more style updates on login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -20,9 +20,9 @@ export function Login() {
       <h1 className="mb-6 text-2xl text-white shadow-lg shadow-black w-full rounded-md p-2 justify-center text-center max-w-sm">
         4292 FALCONS
       </h1>
-      <div className=" shadow-lg shadow-black w-full rounded-md p-2 justify-center text-center max-w-sm">
+      <div className=" w-full rounded-md justify-center text-center max-w-sm">
         <form className="w-full max-w-sm">
-          <div className="my-6 mx-2">
+          <div className="my-6 shadow-lg shadow-black">
             <input
               type="email"
               placeholder="Email"
@@ -33,7 +33,7 @@ export function Login() {
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
           </div>
-          <div className="my-6 mx-2">
+          <div className="my-6 shadow-lg shadow-black">
             <input
               type="password"
               placeholder="Password"
@@ -44,16 +44,16 @@ export function Login() {
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
           </div>
-          <div className="flex items-center justify-end gap-2 mx-2 my-6">
+          <div className="flex items-center justify-end gap-4  my-6 ">
             <button
-              className="ml-4 bg-white hover:bg-purple-100 text-purple-900 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="ml-4 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button"
               onClick={() => user.register(email, password)}
             >
-              Create account
+              Don't have an account?
             </button>
             <button
-              className=" bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="shadow-lg shadow-black bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button"
               onClick={() => user.login(email, password)}
             >
