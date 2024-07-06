18745a7f715d4ad974f1f3dccfe5ef63c69ee4c1
“Arun <arunjacob2@gmail.com>
2024-07-05 23:46:59 -0400

    simplify design

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,47 +13,43 @@ export function Login() {
     if (user.current) {
       navigate("/");
     }
-  }, [navigate, user]);
+  }, [user, navigate]);
 
   return (
-    <section className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-purple-700 via-purple-600 to-purple-700 p-4">
-      <h1 className="mb-6 text-2xl text-white shadow-lg shadow-black w-full rounded-md p-2 justify-center text-center max-w-sm">
+    <section className="flex flex-col items-center justify-center h-screen bg-white p-4">
+      <h1 className="mb-6 text-2xl text-purple-700 w-full rounded-md p-2 text-center max-w-sm">
         4292 FALCONS
       </h1>
-      <div className=" w-full rounded-md justify-center text-center max-w-sm">
-        <form className="w-full max-w-sm">
-          <div className="my-6 shadow-lg shadow-black">
+      <div className="w-full max-w-sm">
+        <form className="w-full">
+          <div className="my-6">
             <input
               type="email"
               placeholder="Email"
               value={email}
-              onChange={(event) => {
-                setEmail(event.target.value);
-              }}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              onChange={(e) => setEmail(e.target.value)}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline"
             />
           </div>
-          <div className="my-6 shadow-lg shadow-black">
+          <div className="my-6">
             <input
               type="password"
               placeholder="Password"
               value={password}
-              onChange={(event) => {
-                setPassword(event.target.value);
-              }}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              onChange={(e) => setPassword(e.target.value)}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline"
             />
           </div>
-          <div className="flex items-center justify-end gap-4  my-6 ">
+          <div className="flex items-center justify-end gap-4 my-6">
             <button
-              className="ml-4 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="text-purple-700 font-bold py-2 px-4 rounded focus:outline-none"
               type="button"
               onClick={() => user.register(email, password)}
             >
               Don't have an account?
             </button>
             <button
-              className="shadow-lg shadow-black bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="shadow-lg bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none"
               type="button"
               onClick={() => user.login(email, password)}
             >
