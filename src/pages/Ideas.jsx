fca18b0f132fa4f371dc86a51e3568fb6936149a
Arun Jacob <ajacin@users.noreply.github.com>
2024-12-13 12:17:56 -0500

    Use proper tailwind color notation

--- src/pages/Ideas.jsx
+++ src/pages/Ideas.jsx
@@ -98,7 +98,7 @@ export function Ideas() {
                   onChange={(event) => setDescription(event.target.value)}
                   className="w-full p-2 mb-4 border rounded"
                 />
-                <span className={description?.length > 500 ? "text-red":"text-black"}
+                <span className={description?.length > 500 ? "text-red-500":"text-black"}
 >{description?.length} characters</span><br/>
                 <button
                   type="button"
